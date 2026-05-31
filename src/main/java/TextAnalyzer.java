e5f51d12827cfbe770e28f070f517663400c44d0
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2026-05-30 21:11:49 -0400

    add feedback

--- src/main/java/TextAnalyzer.java
+++ src/main/java/TextAnalyzer.java
@@ -6,7 +6,7 @@
         */
 import java.util.Scanner;
 import java.util.ArrayList;
-public class ProblemSet {
+public class TextAnalyzer {
 	static Scanner input = new Scanner(System.in);
 
 	public static void main(String args[]) {
